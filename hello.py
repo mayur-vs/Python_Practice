205c72a2040125f1f8c603f43b155dddd98819cb
Mayur <mayurvs.code@gmail.com>
2026-07-25 21:02:50 +0530

    Added favorite food

--- hello.py
+++ hello.py
@@ -3,3 +3,9 @@ print("My favorite movie is:", favorite_movie)
 
 favorite_person = "My Father"
 print("My favorite person is:", favorite_person)
+
+favorite_character = "My Father"
+print("My favorite character is:", favorite_character)
+
+favorite_food = "Pizza"
+print("My favorite food is:", favorite_food)
